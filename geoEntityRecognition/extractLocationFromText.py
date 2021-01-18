bdb055cc8c74feb7f0726eb766db988050192d39
Sepideh Alassi <sepideh.alassi@gmail.com>
2021-01-18 14:05:06 +0100

    extract location from german text

--- geoEntityRecognition/extractLocationFromText.py
+++ geoEntityRecognition/extractLocationFromText.py
@@ -1,8 +1,51 @@
 import spacy
+import os
 
-nlp = spacy.load("en_core_web_sm")
+locationLabels = ['GPE', 'LOC', 'FAC']
 
-doc = nlp("Sepideh Alassi lives in suburbs of Basel and works at DaSch Swiss that is located close to Basel Airport.")
 
-for ent in doc.ents:
-    print(ent.text, ent.label_)
+class GeoEntity:
+    def __init__(self, text, label, startChar, endChar):
+        self.label = label
+        self.text = text
+        self.startChar = startChar
+        self.endChar = endChar
+
+
+def extractLocations(entities):
+    foundLocations = []
+    geoEntities = list(filter(lambda ent: ent.label_ in locationLabels, entities))
+
+    for ent in geoEntities:
+        entity = GeoEntity(ent.text, ent.label_, ent.start_char, ent.end_char)
+        foundLocations.append(entity.__dict__)
+
+    return foundLocations
+
+
+def getLocationsEnglish(text):
+    # load pretrained english model
+    englishModel = spacy.load("en_core_web_sm")
+
+    # extrac geo entities
+    doc = englishModel(text)
+    return extractLocations(doc.ents)
+
+
+def getLocationsGerman(text):
+    # load pretrained german model
+    germanhModel = spacy.load("de_core_news_sm")
+
+    # extrac geo entities
+    doc = germanhModel(text)
+    return extractLocations(doc.ents)
+
+if __name__ == '__main__':
+    testDataPath = "test_data/"
+    englishTestData = os.path.join(testDataPath, "magellan_voyage.txt")
+    englishText = open(englishTestData, "r").read()
+    print(getLocationsEnglish(englishText))
+
+    germanTestData = os.path.join(testDataPath, "mozartItalienReise.txt")
+    germanText = open(germanTestData, "r").read()
+    print(getLocationsGerman(germanText))
