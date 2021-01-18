2911ae4aecee66dabaee98fa9ff1a132de788a22
Sepideh Alassi <sepideh.alassi@gmail.com>
2021-01-18 16:38:15 +0100

    italiand text parser and refactor

--- geoEntityRecognition/extractLocationFromText.py
+++ geoEntityRecognition/extractLocationFromText.py
@@ -1,51 +1,73 @@
 import spacy
 import os
-
-locationLabels = ['GPE', 'LOC', 'FAC']
-
-
-class GeoEntity:
-    def __init__(self, text, label, startChar, endChar):
-        self.label = label
-        self.text = text
-        self.startChar = startChar
-        self.endChar = endChar
+from geoEntity import GeoEntity
 
 
-def extractLocations(entities):
-    foundLocations = []
-    geoEntities = list(filter(lambda ent: ent.label_ in locationLabels, entities))
+class ExtractLocationFormText:
+    def __init__(self):
+        self.locationLabels = ['GPE', 'LOC', 'FAC']
 
-    for ent in geoEntities:
-        entity = GeoEntity(ent.text, ent.label_, ent.start_char, ent.end_char)
-        foundLocations.append(entity.__dict__)
+    def extractLocationEntities(self, entities, lang):
+        foundLocations = []
+        geoEntities = list(filter(lambda ent: ent.label_ in self.locationLabels, entities))
 
-    return foundLocations
+        for ent in geoEntities:
+            entity = GeoEntity(ent.text, ent.label_, ent.start_char, ent.end_char, lang)
+            foundLocations.append(entity.__dict__)
 
+        return foundLocations
 
-def getLocationsEnglish(text):
-    # load pretrained english model
-    englishModel = spacy.load("en_core_web_sm")
+    def getLocationsEnglish(self, text):
+        # load pretrained english model
+        englishModel = spacy.load("en_core_web_sm")
 
-    # extrac geo entities
-    doc = englishModel(text)
-    return extractLocations(doc.ents)
+        # extrac geo entities
+        doc = englishModel(text)
+        return self.extractLocationEntities(doc.ents, 'en')
 
+    def getLocationsGerman(self, text):
+        # load pretrained german model
+        germanhModel = spacy.load("de_core_news_sm")
 
-def getLocationsGerman(text):
-    # load pretrained german model
-    germanhModel = spacy.load("de_core_news_sm")
+        # extrac geo entities
+        doc = germanhModel(text)
+        return self.extractLocationEntities(doc.ents, 'de')
 
-    # extrac geo entities
-    doc = germanhModel(text)
-    return extractLocations(doc.ents)
+    def getLocationsFrench(self, text):
+        # load pretrained french model
+        frenchhModel = spacy.load("fr_core_news_sm")
+
+        # extrac geo entities
+        doc = frenchhModel(text)
+        return self.extractLocationEntities(doc.ents, 'fr')
+
+    def getLocationsItalian(self, text):
+        # load pretrained italian model
+        italianModel = spacy.load("it_core_news_sm")
+
+        # extrac geo entities
+        doc = italianModel(text)
+        return self.extractLocationEntities(doc.ents, 'it')
 
 if __name__ == '__main__':
     testDataPath = "test_data/"
+    parser = ExtractLocationFormText()
     englishTestData = os.path.join(testDataPath, "en_magellan_voyage.txt")
     englishText = open(englishTestData, "r").read()
-    print(getLocationsEnglish(englishText))
+    foundLocations = parser.getLocationsEnglish(englishText)
+    print(len(foundLocations), foundLocations)
 
     germanTestData = os.path.join(testDataPath, "de_mozartItalienReise.txt")
     germanText = open(germanTestData, "r").read()
-    print(getLocationsGerman(germanText))
+    foundLocations = parser.getLocationsGerman(germanText)
+    print(len(foundLocations), foundLocations)
+
+    frenchTestData = os.path.join(testDataPath, "fr_paris_a_Jerusalem.txt")
+    frenchText = open(frenchTestData, "r").read()
+    foundLocations = parser.getLocationsFrench(frenchText)
+    print(len(foundLocations), foundLocations)
+
+    italianTestData = os.path.join(testDataPath, "it_marcOPollo.txt")
+    italianText = open(italianTestData, "r").read()
+    foundLocations = parser.getLocationsItalian(italianText)
+    print(len(foundLocations), foundLocations)
---
+++ geoEntityRecognition/geoEntity.py
@@ -0,0 +1,7 @@
+class GeoEntity:
+    def __init__(self, text, label, startChar, endChar, lang):
+        self.label = label
+        self.text = text
+        self.startChar = startChar
+        self.endChar = endChar
+        self.language = lang
