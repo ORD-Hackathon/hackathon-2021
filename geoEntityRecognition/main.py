e27cc0010c167731ad81cb1fd336eaad2f63387d
Sepideh Alassi <sepideh.alassi@gmail.com>
2021-01-19 12:17:09 +0100

    introduce sleep

--- geoEntityRecognition/main.py
+++ geoEntityRecognition/main.py
@@ -1,6 +1,7 @@
 import os
 from extractLocationFromText import ExtractLocationFromText
 from getIdentifer import getWikiRecord
+import time
 
 
 if __name__ == '__main__':
@@ -10,7 +11,13 @@ if __name__ == '__main__':
     englishTestData = os.path.join(testDataPath, "en_magellan_voyage.txt")
     englishText = open(englishTestData, "r").read()
     foundLocations = parser.getLocationsEnglish(englishText)
+    counter = 0
     for loc in foundLocations:
         loc.wikiID, loc.geonameID, loc.longitude, loc.latitude = getWikiRecord(loc.text, loc.language)
+        if (counter>10):
+            time.sleep(60)
+            counter = 0
+        else :
+            counter += 1
         print(loc.__dict__)
 
