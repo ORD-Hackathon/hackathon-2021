7e5259ca402d7bfd41802aa3f3a12b5981c66d56
Sepideh Alassi <sepideh.alassi@gmail.com>
2021-01-19 11:20:09 +0100

    fix import

--- geoEntityRecognition/main.py
+++ geoEntityRecognition/main.py
@@ -1,15 +1,16 @@
 import os
-from extractLocationFromText import ExtractLocationFormText
+from extractLocationFromText import ExtractLocationFromText
 from getIdentifer import getWikiRecord
 
 
 if __name__ == '__main__':
 
     testDataPath = "test_data/"
-    parser = ExtractLocationFormText()
+    parser = ExtractLocationFromText()
     englishTestData = os.path.join(testDataPath, "en_magellan_voyage.txt")
     englishText = open(englishTestData, "r").read()
     foundLocations = parser.getLocationsEnglish(englishText)
     for loc in foundLocations:
         loc.wikiID, loc.geonameID, loc.longitude, loc.lattitude = getWikiRecord(loc.text, loc.language)
         print(loc.__dict__)
+
