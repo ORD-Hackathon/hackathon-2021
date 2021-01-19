0bbd53257bf7b170089fe994c60c0ad94a54050a
Muhammad Jarir Kanji <m.j.kanji@outlook.com>
2021-01-19 10:07:23 +0000

    minor updates.

--- geoEntityRecognition/xml_parsing/parse_xml.py
+++ geoEntityRecognition/xml_parsing/parse_xml.py
@@ -1,8 +1,9 @@
 from lxml import etree as et
 
-tree = et.parse('test.xml')
+tree = et.parse('cities_example.xml')
 root = tree.getroot()
 # print(et.tostring(root, pretty_print=True).decode())
-for element in root:
-    for subelement in element:
-        print(subelement.tag, subelement.text)
+print(root.tag)
+# for element in root:
+#     for subelement in element:
+#         print(subelement.tag, subelement.text)
