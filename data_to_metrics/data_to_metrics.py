5e884cd1309d76c2bbccce51d8cb431c229d99e7
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2021-01-19 18:18:28 +0100

    First attempt at integrating all steps

---
+++ data_to_metrics/data_to_metrics.py
@@ -0,0 +1,17 @@
+
+## @Ivan: What format should the dataset be in? Can't always use DOI as unique identifier, as some daataset recrods won't have DOIs.
+
+def dataset_to_score(dataset):
+    """Returns Openness Score for a single dataset record"""
+    pass
+
+def dataset_to_usage_stats(dataset):
+    """Returns Usage stats for a single dataset record"""
+    pass
+
+def datasets_to_scores(list_of_datasets):
+    """Returns aggregated metrics for a list of datasets"""
+    # Uses both funtions above
+    pass
+
+    
---
+++ publication_to_data/webscrape_datasets_from_publication.py
@@ -0,0 +1,43 @@
+## So far format only for Nature journals.
+
+def get_datasets_from_publication(doi):
+    """Returns list of datasets (in Whatever format IVAN wants, from a paper's doi."""
+    ## TO DO !
+    
+    pass
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+​
+#DOI = 'https://www.nature.com/articles/s41467-020-18551-0'
+#DOI = 'https://www.nature.com/articles/s41467-020-20688-x'
+DOI = 'https://science.sciencemag.org/content/366/6462/255'
+​
+html = urlopen(DOI)
+​
+soup = BeautifulSoup(html, "html.parser")
+​
+url = []
+#for nature
+table = soup.findAll('div',attrs={"id":"data-availability-content"})
+for x in table:
+	myString = (x.find('p').text)
+	url += re.findall(r'(https?://\S+)', myString)
+​
+​
+table = soup.findAll('div',attrs={"id":"code-availability-content"})
+for x in table:
+	myString = (x.find('p').text)
+	#print (myString)
+	url += re.findall(r'(https?://\S+)', myString)
+​
+#for science
+table = soup.findAll('div',attrs={"class":"ack"})
+myString = str(table)
+url += re.findall(r'(https?://\S+)', myString)
+​
+​
+print (url)
+Collapse
+
